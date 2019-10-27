2a3ccde8e4caf1cf03d82e6c8c2ec0357a2a34db
Aaron Saunders <ASaunders2@my.wctc.edu>
2019-10-26 20:49:03 -0500

    Add project files.

---
+++ ListQuiz/ListQuiz/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListQuiz
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int score = 0;
+        public void OnMore(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            if (score < 5)
+            {
+                score++;
+                DisplayAlert("Assassin Action", mi.CommandParameter + "your assassin score increased to " + score, "OK");
+            }
+            else
+            {
+                score++;
+                DisplayAlert("Assassin Action", mi.CommandParameter + "your assassin score is max", "OK");
+            }
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            if (score != 0)
+            {
+                score--;
+                DisplayAlert("Templar Action", mi.CommandParameter + "your templar score decreased to " + score, "OK");
+            }
+            else
+            {
+                DisplayAlert("Templar Action", mi.CommandParameter + "your templar score is max", "OK");
+            }
+        }
+    }
+}
---
+++ ListQuiz/ListQuiz/QuizViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListQuiz
+{
+    public class QuizViewModel
+    {
+        public QuizViewModel(string question)
+        {
+            Question = question;
+        }
+
+        public string Question { get; set; }
+
+        static QuizViewModel()
+        {
+            All = new List<QuizViewModel>{
+                new QuizViewModel("Ezio is our ally"),
+                new QuizViewModel("Vivere is our ally"),
+                new QuizViewModel("Leonardo is our ally"),
+                new QuizViewModel("The Creed is our ally"),
+                new QuizViewModel("Templars are our ally")
+            };
+        }
+        public static IList<QuizViewModel> All { private set; get; }
+    }
+}
